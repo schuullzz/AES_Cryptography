37153e57048b740ce62b02a1d200d3ac9261d72d
Timothy Schultz <59267968+schuullzz@users.noreply.github.com>
2021-07-10 18:58:52 -0500

    Commit #1: Starting

---
+++ Task1.py
@@ -0,0 +1,57 @@
+# Task 1: This is a test to make sure you can compile some
+# code and complete a rudimentary task. Examples were taken
+# from the documentation for pycryptodome.
+# URL: https://www.pycryptodome.org/en/latest/src/examples.html#encrypt-data-with-aes
+
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+
+
+def aes_example():
+    print("AES Example:")
+    message = "Hello from Mars!"
+    key = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(message.encode())
+    print("Ciphertext: ", ciphertext)
+
+    file_out = open("encrypted.bin", "wb")
+    [file_out.write(x) for x in (cipher.nonce, tag, ciphertext)]
+    file_out.close()
+
+    file_in = open("encrypted.bin", "rb")
+    nonce, tag, ciphertext = [file_in.read(x) for x in (16, 16, -1)]
+
+    # let's assume that the key is somehow available again
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    message = cipher.decrypt_and_verify(ciphertext, tag).decode()
+
+    print("Plaintext: ",message)
+
+
+def rsa_example():
+    print("RSA Example:")
+    code = "Pierre"
+    key = RSA.generate(2048)
+    encrypted_key = key.export_key(passphrase=code, pkcs=8, protection="scryptAndAES128-CBC")
+
+    file_out = open("rsa.bin", "wb")
+    file_out.write(encrypted_key)
+    file_out.close()
+
+    print(key.public_key().export_key())
+
+    code = "Pierre"
+    encoded_key = open("rsa.bin", "rb").read()
+    key = RSA.import_key(encoded_key, passphrase=code)
+
+    print(key.publickey().export_key())
+
+
+def sha_256_example():
+    print("SHA-256 Example:")
+    hash_1 = SHA256.new()
+    hash_1.update(b'Greetings from Jupiter')
+    print(hash_1.hexdigest())
---
+++ main.py
@@ -0,0 +1,39 @@
+# Timothy Schultz
+# Professor Hauschild
+# CS 4732
+# 07/10/21
+
+import Task1
+import Task2
+
+# *******************************************************************************
+
+print("Project 2: Cryptography\n")
+
+while True:
+    print("Select an option:")
+    print("1. Task 1")
+    print("2. Task 2")
+    print("3. Quit\n")
+
+    answer = int(input("Option: "))
+    print("")
+
+    if answer == 1:
+        Task1.aes_example()
+        print("")
+        Task1.rsa_example()
+        print("")
+        Task1.sha_256_example()
+        print("")
+    elif answer == 2:
+        Task2.encrypt_decrypt_128()
+        Task2.encrypt_decrypt_256()
+        print("")
+    elif answer == 3:
+        break
+    else:
+        print("Incorrect option try again.\n")
+
+
+
